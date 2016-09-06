b5e03487d5514a4d1b7ab11d140215a821ec1272
sashimii <stailor@Metroland-Administrators-MacBook-Pro-3.local>
2016-09-06 18:20:03 -0400

    Checking if heroku's deployments truly works

--- app.js
+++ app.js
@@ -257,6 +257,8 @@ function receivedMessage(event) {
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
     switch (messageText) {
+      case 'testing this':
+        sendTextMessage(senderID, '"' + messageText + '" works!');
       case 'image':
         sendImageMessage(senderID);
         break;
