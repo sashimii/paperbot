037d9102d4fe993c7804305f9b2991fb7361cd3a
sashimii <stailor@Metroland-Administrators-MacBook-Pro-3.local>
2016-09-09 12:56:35 -0400

    Including Payload, which I forgot to do beforehand

--- app.js
+++ app.js
@@ -437,7 +437,7 @@ function receivedPostback(event) {
 
   // When a postback is called, we'll send a message back to the sender to
   // let them know it was successful
-  data.getSection().then((sectionItems) => {
+  data.getSection(payload).then((sectionItems) => {
     send(sectionItems).to(senderID);
   })
   // sendTextMessage(senderID, "Postback called");
