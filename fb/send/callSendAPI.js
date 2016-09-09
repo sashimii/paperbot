0911547b52ccc8b27c8e39a2dbdd77e196fc0af0
sashimii <stailor@Metroland-Administrators-MacBook-Pro-3.local>
2016-09-09 01:05:17 -0400

    Fixed the boo boo

--- fb/send/callSendAPI.js
+++ fb/send/callSendAPI.js
@@ -1,10 +1,11 @@
 const request = require('request');
 
+const PAGE_ACCESS_TOKEN = process.env.MESSENGER_PAGE_ACCESS_TOKEN || '';
+
 module.exports = function callSendAPI(messageData) {
-  console.log('***PAT***', process.env.PAGE_ACCESS_TOKEN);
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+    qs: { access_token: PAGE_ACCESS_TOKEN },
     method: 'POST',
     json: messageData
 
