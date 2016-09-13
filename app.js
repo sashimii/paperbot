0804782e1d72b17df6b465246517cb9e0af279b7
sashimii <stailor@Metroland-Administrators-MacBook-Pro-3.local>
2016-09-13 16:12:44 -0400

    data is stringified

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ app.get('/webhook', function(req, res) {
  */
 app.post('/webhook', function (req, res) {
   var data = req.body;
-  console.log('***webhook data***', data);
+  console.log('***webhook data***', JSON.stringify(data));
 
   // Make sure this is a page subscription
   if (data.object == 'page') {
