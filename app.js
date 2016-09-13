8f0f9e2b069473f1e863980a009e420364868d77
sashimii <stailor@Metroland-Administrators-MacBook-Pro-3.local>
2016-09-13 16:11:23 -0400

    Taking a look at the data fed into /webhook

--- app.js
+++ app.js
@@ -144,6 +144,7 @@ app.get('/webhook', function(req, res) {
  */
 app.post('/webhook', function (req, res) {
   var data = req.body;
+  console.log('***webhook data***', data);
 
   // Make sure this is a page subscription
   if (data.object == 'page') {
