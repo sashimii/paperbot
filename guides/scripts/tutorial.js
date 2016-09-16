11abf484cf3534ca8d81cb7223f0b4d1e3c34e69
sashimii <stailor@Metroland-Administrators-MacBook-Pro-3.local>
2016-09-16 17:57:56 -0400

    Basic bot guided convo testing

--- guides/scripts/tutorial.js
+++ guides/scripts/tutorial.js
@@ -11,19 +11,24 @@ let tutorial = {
     {
       introduction: (userId) => {
          send(msg.text('Hi, developer friend!')).to(userId);
-         modeManager.nextState(userId);
+        //  modeManager.nextState(userId);
       }
     },
     {
-      persistentMenu: () => {
-        return 'wassup';
+      persistentMenu: (userId) => {
+        send(msg.text('We will talk about the Persistent Menu Here')).to(userId);
       }
     },
     {
-      contentTypes: () => {}
+      contentTypes: (userId) => {
+        send(msg.text('We will talk about various content types next')).to(userId);
+      }
     },
     {
-      staticImages: () => {}
+      staticImages: (userId) => {
+        send(msg.text('Let me show you a static image!')).to(userId);
+        send(msg.image('https://img.grouponcdn.com/deal/iTzVW4nyCypjLANEjKKF/YC-2048x1229.jpg/v1/c700x420.jpg')).to(userId);
+      }
     },
     {
       pngFiles: () => {}
